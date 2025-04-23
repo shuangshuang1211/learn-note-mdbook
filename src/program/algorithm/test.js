4bc3e81f51ba0a6a466d938b4c619dc2a536f54c
liushuangyi <liushuangyi@bytedance.com>
2025-04-23 11:11:51 +0800

    feat: add new question

--- src/program/algorithm/test.js
+++ src/program/algorithm/test.js
@@ -1,34 +1,34 @@
-Array.prototype.myReduce = function (callFn, initVal) {
-  const arr = this;
-  let acc = initVal !== undefined ? initVal : arr[0];
-  let startIndex = initVal !== undefined  ? 0 : 1;
-  console.log("++++ ", acc, arr);
+// Array.prototype.myReduce = function (callFn, initVal) {
+//   const arr = this;
+//   let acc = initVal !== undefined ? initVal : arr[0];
+//   let startIndex = initVal !== undefined  ? 0 : 1;
+//   console.log("++++ ", acc, arr);
   
-  for (let i = startIndex; i< arr.length; i++) {
+//   for (let i = startIndex; i< arr.length; i++) {
     
-    acc = callFn.call(this, acc, arr[i], i, arr)
-  }
+//     acc = callFn.call(this, acc, arr[i], i, arr)
+//   }
   
-  return acc;
-}
+//   return acc;
+// }
 
-const sumArr = [1,2,3].myReduce((acc, cur) => cur + acc, 0)
-// console.log('++++ sumArr', sumArr);
+// const sumArr = [1,2,3].myReduce((acc, cur) => cur + acc, 0)
+// // console.log('++++ sumArr', sumArr);
 
 
 
-function maxStackSize() {
-  // TODO
-  let count = 0;
-  try {
-    (function recurse() {
-      count++;
-      recurse();
-    })();
-  } catch(e) {
-    console.log('++++ e', e);
-    return count;
-  }
-}
+// function maxStackSize() {
+//   // TODO
+//   let count = 0;
+//   try {
+//     (function recurse() {
+//       count++;
+//       recurse();
+//     })();
+//   } catch(e) {
+//     console.log('++++ e', e);
+//     return count;
+//   }
+// }
 
-console.log(maxStackSize())
+// console.log(maxStackSize())
