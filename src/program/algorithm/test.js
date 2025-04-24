f194f8a2ccbe219be7bafb026b7bfcb9645f47ed
liushuangyi <liushuangyi@bytedance.com>
2025-04-24 16:58:39 +0800

    feat: add new content

--- src/program/algorithm/test.js
+++ src/program/algorithm/test.js
@@ -34,11 +34,66 @@
 // console.log(maxStackSize())
 
 // 实现并发控制
+// function parallel(taskItems = [], taskHandler, concurrency = 3) {
+
+// }
+// const task = item => new Promise(resolve => setTimeout(() => {console.log(item); resolve(item)}, 2000))
+
+// parallel([1, 2, 3, 4, 5, 6], task, 2).then((result) => {
+//   console.log('+++', result)
+// })
+
+// 实现并发控制
 function parallel(taskItems = [], taskHandler, concurrency = 3) {
+  // 实现并发控制
+  let currConcurrency = 0;
+  let nextIndex = 0;
+  const len = taskItems.length;
+  const result = new Array(len);
+
+  return new Promise((res, rej) => {
+    // 没有超过concurreny
+    function exectueFn() {
+      while (currConcurrency < concurrency && nextIndex < len) {
+        const currTaskItem = taskItems[nextIndex];
+        let index = nextIndex;
+        console.log('+++++ currTaskItem', currTaskItem);
+        
+        try {
+          taskHandler(currTaskItem)
+            .then((val) => {
+                console.log('++++ handler then', index);
+                result[index] = val;
+            }).catch((err) => {
+              result[index] = err;
+            }).finally(() => {
+              exectueFn();
+              currConcurrency--;
+              if (currConcurrency === 0 && nextIndex === 6) {
+                res(result);
+              }
+            })
+        } catch (err) {
+          result[index] = err;
+        }
+        nextIndex++;
+        currConcurrency++;
+      }
+      
+    }
+    
+    if (len === 0) {
+      res(result)
+    } else {
+      exectueFn();
+    }
+    
+    
+  });
 
 }
 const task = item => new Promise(resolve => setTimeout(() => {console.log(item); resolve(item)}, 2000))
 
 parallel([1, 2, 3, 4, 5, 6], task, 2).then((result) => {
-  console.log('+++', result)
+  console.log('++++', result)
 })
